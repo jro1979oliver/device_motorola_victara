e95763e1b435b2c995c6be4c9e357a0aa1655361
Bruno Martins <bgcngm@gmail.com>
2018-01-26 11:27:35 +0000

    victara: Binderize them all
     * Switch to binderized HAL services as possible and update
       HIDL manifest accordingly.
    
     * Following considerations regarding those without any change:
    
       - Sensors left out of the game because wouldn't work
         otherwise.
    
    Change-Id: I55ea69049c1d5f3ec28ab481a5c6681d4b2e3ccf

--- device.mk
+++ device.mk
@@ -58,6 +58,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
 # Boot animation
@@ -101,6 +102,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
     copybit.msm8974 \
+    android.hardware.memtrack@1.0-service \
     gralloc.msm8974 \
     hwcomposer.msm8974 \
     libgenlock \
