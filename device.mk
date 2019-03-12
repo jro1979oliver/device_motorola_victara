c548e2e5b737d23e6eac860c8e5479c3c05607c3
Brad Ebinger <breadley@google.com>
2019-03-11 17:38:47 -0700

    victara: Enable FEATURE_TELEPHONY_IMS for this device.
    Bug: 118823723
    Test: checkbuild
    Change-Id: Ic026d63d9bf7b59e2501d095799508d7232cad3f
    (cherry picked from commit a551d73d2350956d3a4385fc34d9856a0e40dbf6)

--- device.mk
+++ device.mk
@@ -35,6 +35,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:system/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
