38258079f3ece00382ad07cae3172d764d6c0d2a
invisiblek <dan.pasanen@gmail.com>
2020-01-16 10:11:51 -0300

    victara: Exclude lineage-sdk overlays from RRO * lineage-sdk overlays don't work with RRO

--- lineage_victara.mk
+++ lineage_victara.mk
@@ -21,6 +21,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay/packages/apps/Snap
+    $(LOCAL_PATH)/overlay/packages/apps/Snap\
+	$(LOCAL_PATH)/overlay/lineage-sdk
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
