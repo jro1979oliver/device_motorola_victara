6b609606403a19d3668e581a447dfbf956a70474
jro1979oliver <jroliveira.oliveira301@gmail.com>
2020-04-08 23:35:39 +0000

    Revert "victara: Disable codec2"
    This reverts commit a5afd7c80cd0e4b40adab1a6a12c968a60a2e4db.

--- system_prop.mk
+++ system_prop.mk
@@ -55,7 +55,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # MEDIA
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.stagefright.ccodec=0 \
     debug.stagefright.omx_default_rank.sw-audio=1 \
     debug.stagefright.omx_default_rank=0 \
     persist.media.treble_omx=false
