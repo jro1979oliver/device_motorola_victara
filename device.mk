e049fca7b734ac2718623c3989a7ee24a3c5714e
LuK1337 <priv.luk@gmail.com>
2020-10-01 11:52:48 +0100

    victara: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- device.mk
+++ device.mk
@@ -130,6 +130,12 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
+
 # Init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
