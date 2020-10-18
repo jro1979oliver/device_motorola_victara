1455d8a60e367ef410d4e2c30605391cb3e6a7b5
GuaiYiHu <guaiyihu@foxmail.com>
2020-10-18 16:32:54 +0800

    victara: Build configstore hidl service
    Change-Id: Id75982d9d3d705220032f86b4e8bd97084679dcb

--- device.mk
+++ device.mk
@@ -94,6 +94,7 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.1-service \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
